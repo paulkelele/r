b6a7a6ce80934ca6c3a908c26c51d2727e9d03e6
stephane <paulkelele@gmail.com>
2023-02-22 21:21:00 +0100

    ddd

--- src/r.py
+++ src/r.py
@@ -1,8 +1,52 @@
+import numpy as np
 import pickle
 import pathlib
 import os
 
-states_value = {'b':1}
+ROWS = 3
+COLS = 3
+
+class Agent():
+    def __init__(self,name,exp_rate=0.3) -> None:
+        self.name = name
+        self.exp_rate = exp_rate
+        self.lr = 0.2
+        self.decay_gamma = 0.9
+        self.etat = []
+        self.states_value = {}
+        
+    def flattenBoard(self, board):
+        flattenBoard = str(board.reshape(ROWS, COLS))
+        return flattenBoard
+    
+    def addState(self, state):
+        self.states.append(state)
+    
+    def chooseAction(self,positions,board,symbol):
+        if np.ramdom.uniform(0,1) <= self.exp_rate:
+            idx = np.random.choice(len(positions))
+            action = positions[idx]
+        else:
+            max_value = -999
+            for p in positions:
+                next_board = board.copy()
+                next_board[p] = symbol
+                new_board_flatten = self.flattenBoard(next_board)
+                value = 0 if self.states_value.get(new_board_flatten) is None else self.states_value.get(new_board_flatten)
+                if value >= max_value:
+                    max_value = value
+                    action = p
+        return action
+
+    def feedReward(self, reward):
+        for st in reversed(self.states):
+            pass
+    
+class Environement:
+    def __init__(self) -> None:
+        pass
+
+states_value = {'b':60, 'c':20}
 
 def save(states_value):
     f = pathlib.Path('p')
