c623532338ad2b1652a6347b90af28420036ddfa
stephane boyer <stephane.boyer@wraptor.fr>
2023-02-24 14:59:53 +0100

    add *

--- src/r.py
+++ src/r.py
@@ -2,6 +2,7 @@
 # https://towardsdatascience.com/implement-grid-world-with-q-learning-51151747b455
 # https://towardsdatascience.com/reinforcement-learning-with-sarsa-a-good-alternative-to-q-learning-algorithm-bf35b209e1c
 # https://towardsdatascience.com/reinforcement-learning-rl-101-with-python-e1aa0d37d43b
+# https://www.youtube.com/watch?v=UFqaCkTZ65w
 
 import numpy as np
 import pickle
